832c0b2e561fda9c1fb912c28d47c8031ff198b8
Harsh-Kb <KAMBOJHARSH013@GMAIL.COM>
2025-07-15 19:45:48 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/harsh-kamboj-nexus/',
   server: {
     host: "::",
     port: 8080,
